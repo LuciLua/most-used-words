b6ef987c7c12ac65d363cfbd724cd72f39968316
LuciLua <luci.lua81@gmail.com>
2022-04-10 18:46:39 -0300

    :star2: [502] Interface Gráfica com Vue JS #02

--- src/components/HomeApp.vue
+++ src/components/HomeApp.vue
@@ -30,4 +30,10 @@ export default {
 </script>
 
 <style>
+  .pills{
+    display: flex;
+    flex-wrap: wrap;
+
+    justify-content: space-between;
+  }
 </style>
--- src/components/PillComp.vue
+++ src/components/PillComp.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <span>
+  <div class="pill">
+    <span class="name">
       {{ name }}
+    </span>
+    <span class="amount">
       {{ amount }}
     </span>
   </div>
@@ -13,5 +15,43 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+/* tag scoped define que esse estilo so será aplicado para esse componente*/
+.pill {
+  background: rgb(212, 21, 59);
+  color: #fff;
+
+  margin: 10px;
+  border-radius: 30px;
+  height: 55px;
+
+  font-size: 1.3rem;
+
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+}
+
+.pill .name {
+  margin-left: 15px;
+}
+.pill .amount {
+  margin: 10px;
+  padding: 5px;
+
+  border-radius: 18px;
+
+  background: #fff;
+  color: rgb(212, 21, 59);
+
+  height: 35px;
+  width: 35px;
+
+  font-size: 13px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
