f6cab0358c9f3a9bf9ebaa1a98ed60cb5ad4d206
LuciLua <luci.lua81@gmail.com>
2022-04-11 16:44:05 -0300

    :star2: [506] Processando arquivos #01

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,18 @@
 const { ipcMain } = require('electron')
 
+const pathsToRows = require('./pathsToRows')
+
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths)
 
-    event.reply('process-subtitles', [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 850 },
-    ])
+    pathsToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: "i", amount: 1234 },
+                { name: "you", amount: 900 },
+                { name: "he", amount: 850 },
+            ])
+        })
+
 })
